7c57a36d07ad5040f62fa5a9d1b53971495521ab
Behruz1227 <behruzxonniyozov12@gmail.com>
2023-12-22 12:29:49 +0500

    sxsw

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -5,4 +5,11 @@ export const api = "https://lostfound.pythonanywhere.com/";
 // parol:
 // 22
 
+// background: rgb(255, 255, 255);
+//   background: linear-gradient(
+//     0deg,
+//     rgba(255, 255, 255, 1) 0%,
+//     rgba(34, 129, 195, 1) 85%
+//   );
+
 export const byId = (id) => document.getElementById(id);
